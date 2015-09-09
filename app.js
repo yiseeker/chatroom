87fade0cb92fbcb6fd34c84641e47f122ae10b96
linux <linux@MACHINE.(none)>
2015-09-09 17:34:15 +0800

    2015-09-09 Multi-Peer connection test succeed, but the the memory consume is TRAGEDY.

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+//*********************************************
+//Variable Region Starts
+var clientList=new Array();
 var express = require('express');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var path=require('path');
-
 var app = express();
-
 var server=require('http').Server(app);
 var io=require('socket.io').listen(server);
+//Variable Region Ends
+//*********************************************
 
-// view engine setup
+
+
+//*********************************************
+//Setting Region Starts
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -49,10 +55,17 @@ app.use(function(err, req, res, next) {
 });
 
 //maintain a online client list
-var clientList=new Array();
+
 io.on('connection',function(socket){
     socket.on('disconnect',function(){
-        console.log('user leave');
+        var room=GetRoomBySocketId(socket.id);
+        if(room!='')
+        {
+            socket.broadcast.to(room).emit('_offline',socket.id);
+        }
+        clientList.Remove(socket.id);
+        console.log('user leave, remove socketid and emmit to room: '+socket.id);
+        Traversal();
     });
 
 
@@ -84,22 +97,112 @@ io.on('connection',function(socket){
     socket.on('_message',function(message){
         if(message.type=='offer')
         {
-
+            if(IsSocketOnLine(message.to))//if destination socket still online,send offer to it
+            {
+                io.sockets.connected[message.to].emit('_message',message);
+                console.log('[emit offer] from : '+message.from+' to : '+message.to);
+            }
+            else//if socket offline, remove from the clientList array
+            {
+                var sid=message.to;
+                socket.emit('_offline',sid);
+                clientList.Remove(sid);
+                console.log('[user offline] : '+sid);
+            }
         }
         else if(message.type=='answer')
         {
-
+            if(IsSocketOnLine(message.to))//if destination socket still online,send offer to it
+            {
+                io.sockets.connected[message.to].emit('_message',message);
+                console.log('[emit answer] from : '+message.from+' to : '+message.to);
+            }
+            else//if socket offline, remove from the clientList array
+            {
+                var sid=message.to;
+                socket.emit('_offline',sid);
+                clientList.Remove(sid);
+                console.log('[user offline] : '+sid);
+            }
         }
         else if(message.type=='candidate')
         {
-
+            socket.broadcast.to(GetRoomBySocketId(socket.id)).emit('_message',message);
+            console.log('[emit candidate] from : '+socket.id);
+            //console.log('[CANDIDATE INFO] : '+JSON.stringify(message.data));
         }
     });
-})
+});
+
 
 
 server.listen(3000,function(){
     console.log('app: listening port 3000');
-})
+});
+
+//Setting Region Ends
+//*********************************************
+
+
+
+///Function Region Start
+function IsSocketOnLine(sid){
+    for(var i in clientList)
+    {
+        if(clientList[i].socketid==sid)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//define remove method of Array Prototype
+Array.prototype.Remove=function(val)
+{
+    var index=-1;
+    for(var i in clientList)
+    {
+        if(clientList[i].socketid==val)
+        {
+            index=i;
+            break;
+        }
+    }
+    if(index>-1)
+    {
+        this.splice(index,1);
+    }
+};
+
+function GetRoomBySocketId(sid)
+{
+    for(var i in clientList)
+    {
+        if(clientList[i].socketid==sid)
+        {
+            return clientList[i].room;
+        }
+    }
+    return '';
+}
+
+
+function Traversal()
+{
+    console.log('Current Clients on Server:');
+    for(var i in clientList)
+    {
+        if(typeof(clientList[i].socketid)!='undefined')
+        {
+            console.log('ID='+clientList[i].socketid);
+        }
+    }
+}
+
+
+//Function Region Ends
+
+
 
 module.exports = app;
